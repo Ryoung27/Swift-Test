522aca9827fea7d21d4e2f247eed2f85844c43ed
Ryoung27 <RRYoung89@gmail.com>
2018-09-16 16:43:29 -0500

    first commit

---
+++ Contents.swift
@@ -0,0 +1,26 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, Kayla"
+
+var name: String
+name = "Ronnie"
+name = "Richie"
+
+var age: Int
+age = 28
+var latitude:  Double
+latitude = 36.166667
+
+var longitude: Double
+longitude = -86.783333
+
+var stayOutTooLate: Bool
+stayOutTooLate = true
+
+var nothingInBrain: Bool
+nothingInBrain = true
+
+var missABeat: Bool
+missABeat = false
