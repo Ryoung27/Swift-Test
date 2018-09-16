06c833e811eb66d12e92f46b69e5914a961086ae
Ryoung27 <RRYoung89@gmail.com>
2018-09-16 17:28:09 -0500

    more practice with swift

--- Contents.swift
+++ Contents.swift
@@ -1,4 +1,4 @@
-//: Playground - noun: a place where people can play
+//: Playground noun: a place where people can play
 
 import UIKit
 
@@ -14,7 +14,7 @@ var latitude:  Double
 latitude = 36.166667
 
 var longitude: Double
-longitude = -86.783333
+longitude = 86.783333
 
 var stayOutTooLate: Bool
 stayOutTooLate = true
@@ -24,3 +24,44 @@ nothingInBrain = true
 
 var missABeat: Bool
 missABeat = false
+
+name = "Richie"
+"Your name is \(name)"
+
+"Your name is \(name), you are \(age), and the latitude and longitude are \(latitude) \(longitude)"
+
+var evenNumbers = [2, 4, 6, 8]
+var songs = ["California", "Wild Fire", "Los Angeles"]
+songs[0]
+
+
+
+//var person = [
+//    "first": "Ronnie",
+//    "middle": "Richard",
+//    "last": "Young",
+//    "month": "December",
+//    "website": "rryoung.com"
+//]
+
+//person["middle"]
+//person["month"]
+
+var action: String
+var person = "Richie"
+
+if person == "Richie" {
+    action = "Richard"
+} else if person == "Kayla" {
+    action = "Kay"
+} else {
+    action = "Ronnie"
+}
+
+action
+
+for song in songs {
+    print("My favorite song is \(song)")
+}
+
+
